52c34ca7bb295c2c0c1b9fb2950dc34ef939fd47
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2023-03-27 13:28:50 +0200

    Creation of entity product

---
+++ symfony/src/Entity/Product.php
@@ -0,0 +1,271 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $brand = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $model = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $img_url = null;
+
+    #[ORM\Column]
+    private ?int $price_ht = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $material = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $water_resistance = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $movement = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $case_diameter = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $stock = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $category = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $color = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $reduce = null;
+
+    #[ORM\OneToMany(mappedBy: 'product', targetEntity: OrderItems::class)]
+    private Collection $orderItems;
+
+    public function __construct()
+    {
+        $this->orderItems = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getImgUrl(): ?string
+    {
+        return $this->img_url;
+    }
+
+    public function setImgUrl(string $img_url): self
+    {
+        $this->img_url = $img_url;
+
+        return $this;
+    }
+
+    public function getPriceHt(): ?int
+    {
+        return $this->price_ht;
+    }
+
+    public function setPriceHt(int $price_ht): self
+    {
+        $this->price_ht = $price_ht;
+
+        return $this;
+    }
+
+    public function getMaterial(): ?string
+    {
+        return $this->material;
+    }
+
+    public function setMaterial(string $material): self
+    {
+        $this->material = $material;
+
+        return $this;
+    }
+
+    public function getWaterResistance(): ?int
+    {
+        return $this->water_resistance;
+    }
+
+    public function setWaterResistance(?int $water_resistance): self
+    {
+        $this->water_resistance = $water_resistance;
+
+        return $this;
+    }
+
+    public function getMovement(): ?string
+    {
+        return $this->movement;
+    }
+
+    public function setMovement(?string $movement): self
+    {
+        $this->movement = $movement;
+
+        return $this;
+    }
+
+    public function getCaseDiameter(): ?int
+    {
+        return $this->case_diameter;
+    }
+
+    public function setCaseDiameter(?int $case_diameter): self
+    {
+        $this->case_diameter = $case_diameter;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getReduce(): ?int
+    {
+        return $this->reduce;
+    }
+
+    public function setReduce(?int $reduce): self
+    {
+        $this->reduce = $reduce;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, OrderItems>
+     */
+    public function getOrderItems(): Collection
+    {
+        return $this->orderItems;
+    }
+
+    public function addOrderItem(OrderItems $orderItem): self
+    {
+        if (!$this->orderItems->contains($orderItem)) {
+            $this->orderItems->add($orderItem);
+            $orderItem->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrderItem(OrderItems $orderItem): self
+    {
+        if ($this->orderItems->removeElement($orderItem)) {
+            // set the owning side to null (unless already changed)
+            if ($orderItem->getProduct() === $this) {
+                $orderItem->setProduct(null);
+            }
+        }
+
+        return $this;
+    }
+}
