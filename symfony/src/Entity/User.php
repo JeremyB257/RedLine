02213a56888b0ddd1ef28de9e8b1c7a6621fec9c
Amandine Cian <amandine.cian@gmail.com>
2023-03-28 08:46:08 +0200

    make:user & make:auth

--- symfony/src/Entity/User.php
+++ symfony/src/Entity/User.php
@@ -7,20 +7,22 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $mail = null;
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
+    #[ORM\Column]
+    private array $roles = [];
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $civility = null;
@@ -74,33 +76,28 @@ class User
     {
         $this->orders = new ArrayCollection();
         $this->carts = new ArrayCollection();
+        $this->createdAt = new \DateTimeImmutable();
     }
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getMail(): ?string
+    public function getEmail(): ?string
     {
-        return $this->mail;
+        return $this->email;
     }
 
-    public function setMail(string $mail): self
+    public function setEmail(string $email): self
     {
-        $this->mail = $mail;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
+        $this->email = $email;
 
         return $this;
     }
@@ -274,62 +271,55 @@ class User
     }
 
     /**
-     * @return Collection<int, Order>
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
      */
-    public function getOrders(): Collection
+    public function getUserIdentifier(): string
     {
-        return $this->orders;
+        return (string) $this->email;
     }
 
-    public function addOrder(Order $order): self
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
     {
-        if (!$this->orders->contains($order)) {
-            $this->orders->add($order);
-            $order->setUser($this);
-        }
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
 
-        return $this;
+        return array_unique($roles);
     }
 
-    public function removeOrder(Order $order): self
+    public function setRoles(array $roles): self
     {
-        if ($this->orders->removeElement($order)) {
-            // set the owning side to null (unless already changed)
-            if ($order->getUser() === $this) {
-                $order->setUser(null);
-            }
-        }
+        $this->roles = $roles;
 
         return $this;
     }
 
     /**
-     * @return Collection<int, Cart>
+     * @see PasswordAuthenticatedUserInterface
      */
-    public function getCarts(): Collection
+    public function getPassword(): string
     {
-        return $this->carts;
+        return $this->password;
     }
 
-    public function addCart(Cart $cart): self
+    public function setPassword(string $password): self
     {
-        if (!$this->carts->contains($cart)) {
-            $this->carts->add($cart);
-            $cart->setUserN($this);
-        }
+        $this->password = $password;
 
         return $this;
     }
 
-    public function removeCart(Cart $cart): self
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
     {
-        if ($this->carts->removeElement($cart)) {
-            // set the owning side to null (unless already changed)
-            if ($cart->getUserN() === $this) {
-                $cart->setUserN(null);
-            }
-        }
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
