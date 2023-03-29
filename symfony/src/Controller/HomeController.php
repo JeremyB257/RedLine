ca9994c0bb5f9f630222b4075fe75961fe39d1d3
Jerem <bertelootjerem@gmail.com>
2023-03-29 15:38:40 +0200

    add banner

--- symfony/src/Controller/HomeController.php
+++ symfony/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home.index')]
-    public function index(): Response
+    public function index(ProductRepository $productRepo): Response
     {
-        return $this->render('index.html.twig', []);
+        return $this->render('index.html.twig', [
+            'watch' => $productRepo->findOneBy([])
+        ]);
     }
 }
