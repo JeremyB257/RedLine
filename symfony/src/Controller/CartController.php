851817a8fa3811c52765feca2ceb102e82bebddb
fares berrani <fberrani@hotmail.fr>
2023-03-31 17:32:17 +0200

    panier fonctionel

--- symfony/src/Controller/CartController.php
+++ symfony/src/Controller/CartController.php
@@ -33,6 +33,7 @@ class CartController extends AbstractController
 
         return $this->render('cart/index.html.twig', compact("dataCart", "total"));
     }
+
     #[Route('/cart/add/{id}', name: 'app_add')]
     public function add(Product $product, $id, SessionInterface $session)
     {
@@ -46,6 +47,44 @@ class CartController extends AbstractController
             $cart[$id] = 1;
         }
 
+        // on sauvegarde dans la seesion
+        $session->set('panier', $cart);
+
+        return $this->redirectToRoute("app_cart");
+    }
+
+    #[Route('/cart/remove/{id}', name: 'app_remove')]
+    public function remove(Product $product, $id, SessionInterface $session)
+    {
+        // On recupere le panier actuelle
+        
+        $cart = $session->get('panier', []);
+        $id = $product->getId();
+
+        if (!empty($cart[$id])) {
+            if($cart[$id] > 1){
+                $cart[$id]--;
+            }else{
+                unset($cart[$id]);
+            }
+        }
+        // on sauvegarde dans la seesion
+        $session->set('panier', $cart);
+
+        return $this->redirectToRoute("app_cart");
+    }
+
+    #[Route('/cart/delete/{id}', name: 'app_delete')]
+    public function delete(Product $product, $id, SessionInterface $session)
+    {
+        // On recupere le panier actuelle
+
+        $cart = $session->get('panier', []);
+        $id = $product->getId();
+
+        if (!empty($cart[$id])) {
+                unset($cart[$id]);
+        }
         // on sauvegarde dans la seesion
         $session->set('panier', $cart);
 
