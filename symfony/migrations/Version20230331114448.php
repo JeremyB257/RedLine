a7b4c26302acac1965d4a89872a775f31389f1d3
Jerem <bertelootjerem@gmail.com>
2023-03-31 13:46:21 +0200

    add img

--- symfony/migrations/Version20230331114448.php
+++ symfony/migrations/Version20230331114448.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230330160406 extends AbstractMigration
+final class Version20230331114448 extends AbstractMigration
 {
     public function getDescription(): string
     {
