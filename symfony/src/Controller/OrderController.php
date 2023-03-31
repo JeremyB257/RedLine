44e0d17ae6074ded3054833e4633d9638d102a64
Jerem <bertelootjerem@gmail.com>
2023-03-31 16:23:36 +0200

    add security

--- symfony/src/Controller/OrderController.php
+++ symfony/src/Controller/OrderController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
-    #[Route('/commandes', name: 'order.index')]
-    public function index(): Response
+    #[Route('/{id}commandes', name: 'order.index')]
+    #[Security("is_granted('ROLE_USER') and user === currentUser")]
+    public function index(User $currentUser): Response
     {
         return $this->render('user/order.html.twig', [
-            'user' => $this->getUser()
+            'user' => $currentUser
         ]);
     }
 }
