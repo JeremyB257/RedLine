811c373c6c02912ebbf22941aa8656ebc09c77e2
fares berrani <fberrani@hotmail.fr>
2023-03-31 15:41:46 +0200

    ajout au panier en cours

--- symfony/src/Controller/CartController.php
+++ symfony/src/Controller/CartController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use phpDocumentor\Reflection\Types\Void_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
@@ -14,5 +17,23 @@ class CartController extends AbstractController
         return $this->render('cart/index.html.twig', [
             
         ]);
+    }
+    #[Route('/cart/add/{id}', name: 'app_add')]
+    public function add(Product $product, $id, SessionInterface $session)
+    {
+        // On recupere le pa,ier actuelle
+        $panier = $session->get('panier', []);
+        $id = $product->getId();
+
+        if(!empty($panier[$id])){
+            $panier[$id]++;
+        }else{
+            $panier[$id] = 1;
+        }
+
+        // on sauvegarde dans la seesion
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute("app_cart");
     }
 }
