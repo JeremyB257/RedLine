0a31c2370b2b62ec03b25f3bc65fd52b1084c07f
Jerem <bertelootjerem@gmail.com>
2023-04-01 14:45:56 +0200

    add path

--- symfony/src/Controller/OrderController.php
+++ symfony/src/Controller/OrderController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use App\Entity\User;
 use App\Repository\OrderRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,13 +25,15 @@ class OrderController extends AbstractController
 
 
     #[Route('/{idUser}/commandes/{idOrder}', name: 'order.show')]
-    #[Security("is_granted('ROLE_USER') and user === currentUser")]
-    public function show(User $currentUser, OrderRepository $orderRepo): Response
+    #[Security("is_granted('ROLE_USER') and user === currentUser and user === order.getUser()")]
+    #[ParamConverter('currentUser', options: ['mapping' => ['idUser' => 'id']])]
+    #[ParamConverter('order', options: ['mapping' => ['idOrder' => 'id']])]
+    public function show(User $currentUser, Order $order, OrderRepository $orderRepo): Response
     {
 
         return $this->render('user/orderShow.html.twig', [
             'user' => $currentUser,
-            'orders' => $orderRepo->findBy(['user' => $currentUser], ['createdAt' => 'DESC'])
+            'order' => $orderRepo->findOneBy(['id' => $order], ['createdAt' => 'DESC'])
         ]);
     }
 }
