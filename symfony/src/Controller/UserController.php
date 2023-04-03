6855903c77d95c21762c5dfcd665bac168f8f618
Jerem <bertelootjerem@gmail.com>
2023-04-03 11:35:27 +0200

    init + path

--- symfony/src/Controller/UserController.php
+++ symfony/src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     #[Route('/utilisateur/{id}', name: 'app_user')]
-    public function index(User $user): Response
+    #[Security("is_granted('ROLE_USER') and user === currentUser")]
+    public function index(User $currentUser): Response
     {
         return $this->render('user/index.html.twig', [
-            'user' => $user,
+            'user' => $currentUser,
+        ]);
+    }
+
+
+    #[Route('/utilisateur/{id}/mot-de-passe', name: 'user.password')]
+    #[Security("is_granted('ROLE_USER') and user === currentUser")]
+    public function changePassword(User $currentUser): Response
+    {
+
+        return $this->render('user/password.html.twig', [
+            'user' => $currentUser,
         ]);
     }
 }
