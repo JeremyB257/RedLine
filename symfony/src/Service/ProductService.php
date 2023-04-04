424462c2cb9e6e4d06999878dc7b04f4829ecbd9
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2023-04-04 12:35:53 +0200

    create product service

---
+++ symfony/src/Service/ProductService.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace App\Service;
+
+use App\Repository\ProductRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class ProductService
+{
+    public function __construct(
+        private RequestStack $requestStack,
+        private ProductRepository $productRepository,
+        private PaginatorInterface $paginator,
+    )
+    {
+        
+    }
+    public function getPaginatedProducts($filters)
+    {
+        $request = $this->requestStack->getMainRequest();
+        $page = $request->query->getInt('page', 1);
+        $limit = 2;
+        $productsQuery = $this->productRepository->findByManyFilters($filters);
+
+        return $this->paginator->paginate($productsQuery, $page, $limit);
+    }
+
+    public function getPaginatedProductsSearch($search)
+    {
+        $request = $this->requestStack->getMainRequest();
+        $page = $request->query->getInt('page', 1);
+        $limit = 2;
+        $productsQuery = $this->productRepository->findBySearchTerms($search);
+
+        return $this->paginator->paginate($productsQuery, $page, $limit);
+    }
+
+
+
+}
