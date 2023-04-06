16dd3d0ddbc9facba53561cbfc8d5a160afbc960
fares berrani <fberrani@hotmail.fr>
2023-04-06 11:27:39 +0200

    fix css alert + mailer

--- symfony/src/Controller/ContactController.php
+++ symfony/src/Controller/ContactController.php
@@ -9,11 +9,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request,EntityManagerInterface $entityManager): Response
+    public function index(Request $request,EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
         $contact = new Contact();
 
@@ -21,13 +23,24 @@ class ContactController extends AbstractController
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
-
+            $contact = $form->getData();
             $contact->setCreatedAt(new \DateTimeImmutable());
             
             
             $entityManager->persist($contact);
             $entityManager->flush();
+
+            $email = (new Email())
+            ->from($contact->getEmail())
+            ->to('laxar@laxar.com')
+            ->subject($contact->getSubject())
+            ->html($contact->getMessage());
+
+        $mailer->send($email);
+
             $this->addFlash('success', 'Le message a bien été envoyé ');
+
+
 
             return $this->redirectToRoute('app_contact');
 
