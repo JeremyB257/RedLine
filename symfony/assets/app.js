bc1686ee803c8e3b90c34bff98c141e9f4adc939
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2023-04-07 11:31:34 +0200

    add my fav button

--- symfony/assets/app.js
+++ symfony/assets/app.js
@@ -11,6 +11,12 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
+// import scroll reveal file
+import './scripts/scroll_reveal';
+
+// import fav feature
+import Fav from './scripts/fav.js';
+
 //search navbar
 const searchBtn = document.querySelector('#searchBtn');
 const searchSubmit = document.querySelector('.searchSubmit');
@@ -71,81 +77,13 @@ if (starRatingStars) {
     select.value = e.target.dataset.value;
   });
 }
-// Scroll Reveal Plug-In
 
-// Home page
-let watches = document.querySelectorAll('.watchReveal');
-ScrollReveal().reveal(watches, {
-  interval: 200,
-  reset: true,
-  delay: 150,
-  duration: 600,
-  origin: 'bottom',
-  distance: '50px',
-});
+// Fav feature
 
-let watchHomePage = document.querySelector('.imgReveal');
-ScrollReveal().reveal(watchHomePage, {
-  delay: 150,
-  duration: 700,
-  origin: 'top',
-  distance: '50px',
-});
-
-let textHomePage = document.querySelectorAll('.titleReveal');
-ScrollReveal().reveal(textHomePage, {
-  delay: 300,
-  interval: 150,
-  duration: 700,
-  origin: 'left',
-  distance: '50px',
-});
-
-let reviews = document.querySelector('.reviewReveal');
-ScrollReveal().reveal(reviews, {
-  delay: 300,
-  interval: 150,
-  duration: 700,
-  origin: 'bottom',
-  distance: '50px',
-});
-
-let attributesLeft = document.querySelectorAll('.attrRevealLeft');
-ScrollReveal().reveal(attributesLeft, {
-  delay: 200,
-  duration: 700,
-  origin: 'left',
-  distance: '50px',
-});
-
-let attributesRight = document.querySelectorAll('.attrRevealRight');
-ScrollReveal().reveal(attributesRight, {
-  delay: 300,
-  duration: 700,
-  origin: 'right',
-  distance: '50px',
-});
-
-let xLaxar = document.querySelector('.xLaxarReveal');
-ScrollReveal().reveal(xLaxar, {
-  delay: 400,
-  duration: 400,
-  origin: 'top',
-  distance: '50px',
-});
-
-let LaLaxar = document.querySelector('.laLaxarReveal');
-ScrollReveal().reveal(LaLaxar, {
-  delay: 200,
-  duration: 400,
-  origin: 'left',
-  distance: '50px',
-});
-
-let ArLaxar = document.querySelector('.arLaxarReveal');
-ScrollReveal().reveal(ArLaxar, {
-  delay: 200,
-  duration: 400,
-  origin: 'right',
-  distance: '50px',
+document.addEventListener('DOMContentLoaded', () => {
+  const favElements = [].slice.call(document.querySelectorAll('.fav'));
+  console.log(favElements);
+  if (favElements) {
+    new Fav(favElements);
+  }
 });
---
+++ symfony/assets/scripts/fav.js
@@ -0,0 +1,9 @@
+export default class Fav {
+  constructor(favElements) {
+    this.favElements = favElements;
+
+    if (this.favElements) {
+      console.log(this.favElements);
+    }
+  }
+}
