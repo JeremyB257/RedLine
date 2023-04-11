2e33dd76a6baa4ba3bf718afa5d5f63180a60246
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2023-04-11 11:06:32 +0200

    fav feature done

--- symfony/assets/controllers/fav_controller.js
+++ symfony/assets/controllers/fav_controller.js
@@ -1,7 +1,16 @@
+import axios from 'axios';
 import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
-  connect() {
-    console.log(this.element.innerHTML);
+  favorite(event) {
+    event.preventDefault();
+    const url = this.element.href;
+    axios.get(url).then(response => {
+      const getSvgFilled = this.element.querySelector('svg.filled');
+      const getSvgUnfilled = this.element.querySelector('svg.unfilled');
+
+      getSvgFilled.classList.toggle('hidden');
+      getSvgUnfilled.classList.toggle('hidden');
+    });
   }
 }
